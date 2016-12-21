e53747ff2ad8ed8cd1079d3a02aa3a1f1b6b7ded
mgsalazar6 <maurengsalazar@gmail.com>
2016-12-22 00:10:41 +0800

    sweet

--- exercise5/add_data.php
+++ exercise5/add_data.php
@@ -108,7 +108,7 @@ function test_input($data) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Add</title>
+<title>Addss</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 <div id="header">
 
