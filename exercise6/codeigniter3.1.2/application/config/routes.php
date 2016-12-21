0a63c9614b353b7583acf42196a4e2a32268302f
mgsalazar6 <maurengsalazar@gmail.com>
2016-12-22 02:16:56 +0800

    added some codes routes.php

--- exercise6/codeigniter3.1.2/application/config/routes.php
+++ exercise6/codeigniter3.1.2/application/config/routes.php
@@ -49,6 +49,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'news';
+$route['default_controller'] = 'user';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['user'] = 'user';
+$route['user/create'] = 'user/create';
+ 
+$route['user/edit/(:any)'] = 'user/edit/$1';
+ 
+$route['user/view/(:any)'] = 'user/view/$1';
+$route['user/(:any)'] = 'user/view/$1';
+
