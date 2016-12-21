f46c3330e0f44b5ddb625bd26078b4ccc93a46a7
mgsalazar6 <maurengsalazar@gmail.com>
2016-12-22 02:43:16 +0800

    deleted comments

--- exercise6/codeigniter3.1.2/application/config/user_agents.php
+++ exercise6/codeigniter3.1.2/application/config/user_agents.php
@@ -1,15 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------
-| USER AGENT TYPES
-| -------------------------------------------------------------------
-| This file contains four arrays of user agent data. It is used by the
-| User Agent Class to help identify browser, platform, robot, and
-| mobile device data. The array keys are used to identify the device
-| and the array values are used to set the actual name of the item.
-*/
+/
 $platforms = array(
 	'windows nt 10.0'	=> 'Windows 10',
 	'windows nt 6.3'	=> 'Windows 8.1',
@@ -56,14 +48,12 @@ $platforms = array(
 );
 
 
-// The order of this array should NOT be changed. Many browsers return
-// multiple browser types so we want to identify the sub-type first.
 $browsers = array(
 	'OPR'			=> 'Opera',
 	'Flock'			=> 'Flock',
 	'Edge'			=> 'Spartan',
 	'Chrome'		=> 'Chrome',
-	// Opera 10+ always reports Opera/9.80 and appends Version/<real version> to the user agent string
+
 	'Opera.*?Version'	=> 'Opera',
 	'Opera'			=> 'Opera',
 	'MSIE'			=> 'Internet Explorer',
@@ -91,23 +81,14 @@ $browsers = array(
 );
 
 $mobiles = array(
-	// legacy array, old values commented out
 	'mobileexplorer'	=> 'Mobile Explorer',
-//  'openwave'			=> 'Open Wave',
-//	'opera mini'		=> 'Opera Mini',
-//	'operamini'			=> 'Opera Mini',
-//	'elaine'			=> 'Palm',
+,
 	'palmsource'		=> 'Palm',
-//	'digital paths'		=> 'Palm',
-//	'avantgo'			=> 'Avantgo',
-//	'xiino'				=> 'Xiino',
-	'palmscape'			=> 'Palmscape',
-//	'nokia'				=> 'Nokia',
-//	'ericsson'			=> 'Ericsson',
-//	'blackberry'		=> 'BlackBerry',
-//	'motorola'			=> 'Motorola'
 
-	// Phones and Manufacturers
+	'palmscape'			=> 'Palmscape',
+
+
+
 	'motorola'		=> 'Motorola',
 	'nokia'			=> 'Nokia',
 	'palm'			=> 'Palm',
@@ -151,7 +132,7 @@ $mobiles = array(
 	'open web'		=> 'Open Web',
 	'openweb'		=> 'OpenWeb',
 
-	// Operating Systems
+
 	'android'		=> 'Android',
 	'symbian'		=> 'Symbian',
 	'SymbianOS'		=> 'SymbianOS',
@@ -159,7 +140,7 @@ $mobiles = array(
 	'series60'		=> 'Symbian S60',
 	'windows ce'	=> 'Windows CE',
 
-	// Browsers
+
 	'obigo'			=> 'Obigo',
 	'netfront'		=> 'Netfront Browser',
 	'openwave'		=> 'Openwave Browser',
@@ -169,7 +150,7 @@ $mobiles = array(
 	'opera mobi'	=> 'Opera Mobile',
 	'fennec'		=> 'Firefox Mobile',
 
-	// Other
+
 	'digital paths'	=> 'Digital Paths',
 	'avantgo'		=> 'AvantGo',
 	'xiino'			=> 'Xiino',
@@ -178,7 +159,6 @@ $mobiles = array(
 	'docomo'		=> 'NTT DoCoMo',
 	'o2'			=> 'O2',
 
-	// Fallback
 	'mobile'		=> 'Generic Mobile',
 	'wireless'		=> 'Generic Mobile',
 	'j2me'			=> 'Generic Mobile',
@@ -190,7 +170,7 @@ $mobiles = array(
 	'cellphone'		=> 'Generic Mobile'
 );
 
-// There are hundreds of bots but these are the most common.
+
 $robots = array(
 	'googlebot'		=> 'Googlebot',
 	'msnbot'		=> 'MSNBot',
