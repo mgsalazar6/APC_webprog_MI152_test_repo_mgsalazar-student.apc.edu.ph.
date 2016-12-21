5846aa67d39566ed56521b907fbf289f1fac9af4
mgsalazar6 <maurengsalazar@gmail.com>
2016-12-22 04:08:16 +0800

    added info

--- exercise6/codeigniter3.1.2/application/views/user/view.php
+++ exercise6/codeigniter3.1.2/application/views/user/view.php
@@ -1,15 +1,13 @@
 
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css"/>
 <?php
-echo '<h3>'.$user_item['firstname'].'</h3>';
-echo '<h3>'.$user_item['lastname'].'</h3>';
-echo '<h3>'.$user_item['midname'].'</h3>';
+echo '<h3>'.$user_item['name'].'</h3>';
 echo '<h3>'.$user_item['nickname'].'</h3>';
 echo '<h3>'.$user_item['email'].'</h3>';
-echo '<h3>'.$user_item['homeadd'].'</h3>';
+echo '<h3>'.$user_item['address'].'</h3>';
 echo '<h3>'.$user_item['gender'].'</h3>';
 echo '<h3>'.$user_item['comment'].'</h3>';
-
+echo '<h3>'.$user_item['cellphone'].'</h3>';
 
 
 
