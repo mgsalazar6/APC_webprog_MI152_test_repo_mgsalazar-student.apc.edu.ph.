f7eee26b090ce378b4ab016701f16092be4d5965
mgsalazar6 <maurengsalazar@gmail.com>
2016-12-22 01:55:09 +0800

    styled the header.php

---
+++ exercise6/codeigniter3.1.2/application/views/templates/header.php
@@ -0,0 +1,18 @@
+<html>
+        <head>
+               <style>
+body {
+    background-image: url("../picOne.jpg");
+    background-repeat: no-repeat;
+}
+</style>
+        </head>
+        
+
+
+
+
+        <body>
+ 
+                <h1>User's Database</h1>
+                <p><a href="<?php echo site_url('user'); ?>">HOME</a> | <a href="<?php echo site_url('user/create'); ?>">ADD DATA</a></p>
