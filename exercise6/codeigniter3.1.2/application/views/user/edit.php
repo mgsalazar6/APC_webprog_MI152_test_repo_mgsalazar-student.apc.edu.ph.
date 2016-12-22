ff02cc4d4347f1d19f4fe11a55319bb571ac491a
mgsalazar6 <maurengsalazar@gmail.com>
2016-12-22 11:13:15 +0800

    created the edit.php

--- exercise6/codeigniter3.1.2/application/views/user/edit.php
+++ exercise6/codeigniter3.1.2/application/views/user/edit.php
@@ -1,25 +1,33 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css"/>
-<h2><?php echo $name; ?></h2>
+<h2><?php echo $firstname; ?></h2>
  
 <?php echo validation_errors(); ?>
  
 <?php echo form_open('user/edit/'.$user_item['id']); ?>
     <table>
         <tr>
-            <td><label for="name">Name</label></td>
-            <td><input type="input" name="name" size="50" value="<?php echo $user_item['name'] ?>" /></td>
-		</tr>
+            <td><label for="firstname">First Name</label></td>
+            <td><input type="input" name="firstname" size="50" value="<?php echo $user_item['firstname'] ?>" /></td>
+        </tr>
+        <tr>
+            <td><label for="lastname">Last Name</label></td>
+            <td><input type="input" name="lastname" size="50" value="<?php echo $user_item['lastname'] ?>" /></td>
+        </tr>
+        <tr>
+            <td><label for="midname">Middle Name</label></td>
+            <td><input type="input" name="midname" size="50" value="<?php echo $user_item['midname'] ?>" /></td>
+        </tr>
         <tr>
             <td><label for="nickname">Nickname</label></td>
             <td><input type="input" name="nickname" size="50" value="<?php echo $user_item['nickname'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="email">Email</label></td>
+            <td><label for="email">Email Address</label></td>
             <td><input type="input" name="email" size="50" value="<?php echo $user_item['email'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="address">Address</label></td>
-            <td><input type="input" name="address" size="50" value="<?php echo $user_item['address'] ?>" /></td>
+            <td><label for="homeadd">Home Address</label></td>
+            <td><input type="input" name="homeadd" size="50" value="<?php echo $user_item['homeadd'] ?>" /></td>
         </tr>
         <tr>
             <td><label for="gender">Gender</label></td>
@@ -28,10 +36,6 @@
         <tr>
             <td><label for="comment">Comment</label></td>
             <td><textarea name="comment" rows="10" cols="40"><?php echo $user_item['comment'] ?></textarea></td>
-        </tr>
-        <tr>
-		 <td><label for="cellphone">Cellphone</label></td>
-            <td><input type="input" name="cellphone" size="50" value="<?php echo $user_item['cellphone'] ?>" /></td>
         </tr>
         <tr>
             <td></td>
