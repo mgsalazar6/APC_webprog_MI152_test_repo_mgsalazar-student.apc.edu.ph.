91ff606d8d4e844462581f189570d518061549a2
mgsalazar6 <maurengsalazar@gmail.com>
2016-12-23 17:29:58 +0800

    background changed

--- exercise6/codeigniter/application/views/templates/header.php
+++ exercise6/codeigniter/application/views/templates/header.php
@@ -2,8 +2,7 @@
         <head>
                <style>
 body {
-    background-color: #EE82EE;
-    background-repeat: no-repeat;
+    background-image:url("doto.png") ;
 }
 </style>
         </head>
@@ -15,4 +14,4 @@ body {
         <body>
  
                 <h1>Informations</h1>
-                <p><a href="<?php echo site_url('user'); ?>">Home</a> | <a href="<?php echo site_url('user/create'); ?>">Add infos</a></p>
+                <p><a href="<?php echo site_url('user'); ?>">Home</a> | <a href="<?php echo site_url('user/create'); ?>">Add Infos</a></p>
